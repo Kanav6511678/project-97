8d95bad2f3b1f10953cbbbb582d82841d6d24210
Kanav6511678 <109471791+Kanav6511678@users.noreply.github.com>
2022-07-31 12:09:08 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+   
+	   //Set "player1_name" using localstorage.setItem() function
+      //Set "player2_name" using localstorage.setItem() function
+
+    window.location = "game_page.html";
+}
+
